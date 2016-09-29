4146120f793004d0d374afa4c20530ffd346bc6b
Ajit, Patil(ajipatil) <ajipatil@lm-sjc-11002085.corp.ebay.com>
2016-09-29 11:52:54 -0700

    Initial Commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component}  from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: `
+		<div class="jumbotron">
+		 <h1> Welcome to our app!</h1>
+		<div>
+	`
+})
+
+export class AppComponent{}
